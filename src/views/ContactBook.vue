6636f4093da6a0b249704df179ca5dc1478bc809
User Name <nguyenanhthu55138@gmail.com>
2022-10-22 17:45:12 +0700

    Tao trang thêm  lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -16,7 +16,9 @@
                     <i class="fas fa-redo"></i> Làm mới
                 </button>
                 <button class="btn btn-sm btn-success" @click="goToAddContact">
-                    <i class="fas fa-plus"></i> Thêm mới
+                    
+                        <i class="fas fa-plus"></i> Thêm mới
+
                 </button>
                 <button class="btn btn-sm btn-danger" @click="removeAllContacts">
                     <i class="fas fa-trash"></i> Xóa tất cả
